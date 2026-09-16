2fb8f7bdf6a682df46deec93e1d4e173a74ee371
arko <arkadiusz.kupracz@gmail.com>
2026-09-16 10:18:30 +0100

    randomise gem tween duration

--- project/game/scenes/main_game.gd
+++ project/game/scenes/main_game.gd
@@ -74,7 +74,8 @@ func _on_gem_dropped(_gem_id: int, amount: int, global_position: Vector2):
 	gem.position = camera.to_local(global_position) + get_viewport().get_visible_rect().size / 2.0
 	_ui.add_child(gem)
 	var tween = create_tween()
-	tween.tween_property(gem, ^"position", _gems_texture.position, 0.5) \
+	var duration_variation := randf_range(0.0, 0.2)
+	tween.tween_property(gem, ^"position", _gems_texture.position, 0.5 + duration_variation) \
 			.set_trans(Tween.TRANS_CUBIC) \
 			.set_ease(Tween.EASE_IN)
 	tween.finished.connect(_collect_gem.bind(gem, amount))
