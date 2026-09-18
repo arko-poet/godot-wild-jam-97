8c3268318f70ac60f7c4313ac645c78c5068df01
arko <arkadiusz.kupracz@gmail.com>
2026-09-18 13:40:18 +0100

    fix unpausing via bindable action

--- project/addons/maaacks_game_template/base/nodes/windows/window_container.gd
+++ project/addons/maaacks_game_template/base/nodes/windows/window_container.gd
@@ -51,7 +51,7 @@ func _handle_cancel_input() -> void:
 	close()
 
 func _unhandled_input(event : InputEvent) -> void:
-	if is_visible_in_tree() and event.is_action_pressed("ui_cancel") and ui_cancel_closes:
+	if is_visible_in_tree() and (event.is_action_pressed("ui_cancel") or event.is_action_pressed("pause")) and ui_cancel_closes:
 		_handle_cancel_input()
 		get_viewport().set_input_as_handled()
 
