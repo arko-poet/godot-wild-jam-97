fa2c68db7c12275e634578be9d30de6e233b7e07
arko <arkadiusz.kupracz@gmail.com>
2026-09-19 13:49:55 +0100

    implement drill sound effects

--- project/game/scenes/character/pet.gd
+++ project/game/scenes/character/pet.gd
@@ -15,7 +15,9 @@ func start_mining() -> void:
 func stop_mining() -> void:
 	mining_timer.stop()
 	sprite.play(&"idle")
+	SfxController.stop_drill()
 
 
 func _on_mining_timer_timeout() -> void:
 	mined.emit()
+	SfxController.play_drill()
